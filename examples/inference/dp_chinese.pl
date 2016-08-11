4f2cafda5ec2d3c952f27f8188a03b49645cf8f7
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-08-11 18:06:31 +0200

    dirichlet process examples working

--- examples/inference/dp_chinese.pl
+++ examples/inference/dp_chinese.pl
@@ -31,17 +31,39 @@ Third Edition, Prentice Hall, Figure 15.10 page 587
 :- mc.
 :- begin_lpad.
 
-dp_n_values(N,N,_Alpha,[]):-!.
+dp_n_values(N,N,_Alpha,[],Counts,Counts):-!.
 
-dp_n_values(N0,N,Alpha,[[V]-1|Vs]):-
+dp_n_values(N0,N,Alpha,[[V]-1|Vs],Counts0,Counts):-
   N0<N,
-  dp_value(N0,Alpha,V),
+  dp_value(N0,Alpha,Counts0,V,Counts1),
   N1 is N0+1,
-  dp_n_values(N1,N,Alpha,Vs).
+  dp_n_values(N1,N,Alpha,Vs,Counts1,Counts).
   
-dp_value(NV,Alpha,V):-
-  dp_stick_index(NV,Alpha,I),
+dp_value(NV,Alpha,Counts,V,Counts1):-
+  draw_sample(Counts,NV,Alpha,I),
+  update_counts(0,I,Alpha,Counts,Counts1),
   dp_pick_value(I,NV,V).
+
+
+update_counts(_I0,_I,Alpha,[_C],[1,Alpha]):-!.
+
+
+update_counts(I,I,_Alpha,[C|Rest],[C1|Rest]):-
+  C1 is C+1.
+
+update_counts(I0,I,Alpha,[C|Rest],[C|Rest1]):-
+  I1 is I0+1,
+  update_counts(I1,I,Alpha,Rest,Rest1).
+
+draw_sample(Counts,NV,Alpha,I):-
+  NS is NV+Alpha,
+  maplist(div(NS),Counts,Probs),
+  length(Counts,LC),
+  numlist(1,LC,Values),
+  maplist(pair,Values,Probs,Discrete),
+  take_sample(NV,Discrete,I).
+
+take_sample(_,D,V):discrete(V,D).
 
 dp_pick_value(I,NV,V):-
   ivar(I,IV),
@@ -55,26 +77,6 @@ mean(_,V0,M):gaussian(M,0,V):-V is V0*30.
 
 value(_,M,V,Val):gaussian(Val,M,V).
 
-dp_stick_index(NV,Alpha,I):-
-  dp_stick_index(1,NV,Alpha,I).
-
-dp_stick_index(N,NV,Alpha,V):-
-  stick_proportion(N,Alpha,P),
-  choose_prop(N,NV,Alpha,P,V).
-  
-choose_prop(N,NV,_Alpha,P,N):-
-  pick_portion(N,NV,P).
-
-choose_prop(N,NV,Alpha,P,V):-
-  neg_pick_portion(N,NV,P),
-  N1 is N+1,
-  dp_stick_index(N1,NV,Alpha,V).
- 
-
-
-stick_proportion(_,Alpha,P):beta(P,1,Alpha).
-
-pick_portion(_,_,P):P;neg_pick_portion(_,_,P):1-P.
 
 :- end_lpad.
 obs0([
@@ -86,27 +88,11 @@ obs1([
 7,7,7,7,7,
 3,3,3,3,3]).
 
-
 obs([
 -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
 7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
 3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]).
 
-hist(Samples,NBins,Chart):-
-  mc_sample_arg(dp_stick_index(1,10.0,V),Samples,V,L),
-  histogram(L,NBins,Chart).
-
-hist_repeated_indexes(Samples,NBins,Chart):-
-  repeat_sample(0,Samples,L),
-  histogram(L,NBins,Chart).
-
-repeat_sample(S,S,[]):-!.
-
-repeat_sample(S0,S,[[N]-1|LS]):-
-  mc_sample_arg_first(dp_stick_index(1,1,10.0,V),10,V,L),
-  length(L,N),
-  S1 is S0+1,
-  repeat_sample(S1,S,LS).
 
 hist_val(Samples,NBins,Chart):-
   mc_sample_arg_first(dp_n_values(0,Samples,10.0,V),1,V,L),
@@ -114,33 +100,33 @@ hist_val(Samples,NBins,Chart):-
   histogram(Vs,NBins,Chart).
 
 prior(Samples,NBins,Chart):-
-  mc_sample_arg_first(dp_n_values(0,100,10.0,T),Samples,T,L),
+  mc_sample_arg_first(dp_n_values(0,100,10.0,T,[10.0],_),Samples,T,L),
   maplist(to_list,L,L1),
   append(L1,Vs),
 %  L=[Vs-_],
-  histogram(Vs,NBins,Chart).
+  histogram(Vs,NBins,-8,15,Chart).
 %  
 post(Samples,NBins,Chart):-
   obs0(O),
   maplist(to_val,O,O1),
   length(O1,N),
 %  mc_sample_arg(dp_value(0,10.0,T),Samples,T,L0),
-  mc_lw_sample_arg_log(dp_value(0,10.0,T),dp_n_values(0,N,10.0,O1),Samples,T,L),
+  mc_lw_sample_arg_log(dp_value(0,10.0,[10.0],T,_),dp_n_values(0,N,10.0,O1,[10.0],_),Samples,T,L),
   maplist(keys,L,LW),
   min_list(LW,Min),
   maplist(exp(Min),L,L1),
 %  maplist(to_list,L,L1),
 %  append(L1,Vs),
 %  L=[Vs-_],
-%  densities(L0,L1,NBins,Chart).
   density(L1,-8,15,NBins,Chart).
-
-keys(_-W,W).
 
 exp(Min,L-W,L-W1):- W1 is exp(W-Min).
 to_list(L-W,L1):-
   maplist(app_w(W),L,L1).
 
+keys(_-W,W).
+div(Den,V,P):- P is V/Den.
+pair(A,B,A:B).
 app_w(W,V-_,V-W).
 to_val(V,[V]-1).
 % plot the density of the state at time 1 in case of no observation (prior)
