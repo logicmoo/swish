e8b6898f02ca67dab938c30e6aad20391969729e
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-08-16 15:21:40 +0200

    DP

--- examples/inference/dirichlet_process.pl
+++ examples/inference/dirichlet_process.pl
@@ -1,36 +1,45 @@
 /*
-One-dimensional  Kalman filter. Hidden Markov model with a real
-value as state and a real value as output. The next state is given by
-the current state plus Gaussian noise (mean 0 and variance 2 in this example)
-and the output is given by the current state plus Gaussian noise (mean
-0 and variance 1 in this example). 
-This example can be considered as modeling a random walk of a single continuous 
-state variable with noisy observations. 
-Given that at time 0 the value 2.5 was
-observed, what is the distribution of the state at time 1 (filtering problem)?
-The distribution of the state is plotted in the case of having (posterior) or 
-not having the observation (prior).
-Liklihood weighing is used to condition the distribution on evidence on
-a continuous random variable (evidence with probability 0).
-CLP(R) constraints allow both sampling and weighing samples with the same
-program.
-From
-Islam, Muhammad Asiful, C. R. Ramakrishnan, and I. V. Ramakrishnan. 
-"Inference in probabilistic logic programs with continuous random variables." 
-Theory and Practice of Logic Programming 12.4-5 (2012): 505-523.
-http://arxiv.org/pdf/1112.2681v3.pdf
-Russell, S. and Norvig, P. 2010. Arficial Intelligence: A Modern Approach. 
-Third Edition, Prentice Hall, Figure 15.10 page 587
+Dirichlet process (DP), see https://en.wikipedia.org/wiki/Dirichlet_process
+Samples are drawn from a base distribution. New samples have a nonzero
+probability of being equal to already sampled values. The process depends
+on a parameter alpha (concentration parameter): with alpha->0, a single 
+value is sampled, with alpha->infinite the distribution is equal to the base
+distribution.
+In this example the base distribution is a Guassian with mean 0 and variance
+1, as in https://en.wikipedia.org/wiki/Dirichlet_process#/media/File:Dirichlet_process_draws.svg
+To model the process, this example uses a stick breaking process: to sample
+a value, a sample beta_1 is taken from Beta(1,alpha) and a coin with heads
+probability beta_1 is flipped. If the coin lands heads, a sample from the base
+distribution is taken and returned. Otherwise, a sample beta_2 is taken again
+from Beta(1,alpha) and a coin is flipped. This procedure is repeated until
+a heads is obtained, the index of i beta_i is the index of the value to be 
+returned.
+The example queries show both the distribution of indexes and values of the DP.
+Moreover, they show the distribution of unique indexes as in 
+http://www.robots.ox.ac.uk/~fwood/anglican/examples/viewer/?worksheet=nonparametrics/dp-mixture-model
+*/
+/** <examples>
+?- hist(200,100,G).
+% show the distribution of indexes with concentration parameter 10. 
+?- hist_val(200,100,G).
+% show the distribution of values with concentration parameter 10. Should look
+% like row 2 of https://en.wikipedia.org/wiki/Dirichlet_process#/media/File:Dirichlet_process_draws.svg
+?- hist_repeated_indexes(100,40,G).
+% show the distribution of unique indexes in 100 samples with concentration parameter 10. 
+
 
 */
-:- use_module(library(mcintyre)).
-:- use_module(library(clpr)).
+ :- use_module(library(mcintyre)).
+
 :- if(current_predicate(use_rendering/1)).
 :- use_rendering(c3).
 :- endif.
 :- mc.
 :- begin_lpad.
 
+% dp_n_values(N0,N,Alpha,L)
+% returns in L a list of N-N0 samples from the DP with concentration parameter
+% Alpha
 dp_n_values(N,N,_Alpha,[]):-!.
 
 dp_n_values(N0,N,Alpha,[[V]-1|Vs]):-
@@ -39,12 +48,20 @@ dp_n_values(N0,N,Alpha,[[V]-1|Vs]):-
   N1 is N0+1,
   dp_n_values(N1,N,Alpha,Vs).
   
+% dp_value(NV,Alpha,V)
+% returns in V the NVth sample from the DP with concentration parameter
+% Alpha
 dp_value(NV,Alpha,V):-
   dp_stick_index(NV,Alpha,I),
   dp_pick_value(I,V).
 
-dp_pick_value(_,V):gaussian(V,0,2).
+% dp_pick_value(I,V)
+% returns in V the value of index I of the base distribution 
+% (in this case N(0,1))
+dp_pick_value(_,V):gaussian(V,0,1).
 
+% dp_stick_index(NV,Alpha,I)
+% returns in I the index of the NVth sample from the DP
 dp_stick_index(NV,Alpha,I):-
   dp_stick_index(1,NV,Alpha,I).
 
@@ -52,6 +69,9 @@ dp_stick_index(N,NV,Alpha,V):-
   stick_proportion(N,Alpha,P),
   choose_prop(N,NV,Alpha,P,V).
   
+% choose_prop(N,NV,Alpha,P,V)
+% returns in V the index of the end of the stick breaking process starting
+% from index N for the NVth value to be sampled from the DP
 choose_prop(N,NV,_Alpha,P,N):-
   pick_portion(N,NV,P).
 
@@ -60,21 +80,13 @@ choose_prop(N,NV,Alpha,P,V):-
   N1 is N+1,
   dp_stick_index(N1,NV,Alpha,V).
  
-
-
+% sample of the beta_i parameters
 stick_proportion(_,Alpha,P):beta(P,1,Alpha).
 
+% flip of the coing for the portion of the stick of size P
 pick_portion(_,_,P):P;neg_pick_portion(_,_,P):1-P.
 
 :- end_lpad.
-obs0([
--1,-1,-1,-1]).
-
-
-obs([
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
-3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]).
 
 hist(Samples,NBins,Chart):-
   mc_sample_arg(dp_stick_index(1,10.0,V),Samples,V,L),
@@ -97,46 +109,5 @@ hist_val(Samples,NBins,Chart):-
   L=[Vs-_],
   histogram(Vs,NBins,Chart).
 
-prior(Samples,NBins,Chart):-
-  mc_sample_arg_first(dp_n_values(0,100,10.0,T),Samples,T,L),
-  maplist(to_list,L,L1),
-  append(L1,Vs),
-%  L=[Vs-_],
-  histogram(Vs,NBins,Chart).
-%  
-post(Samples,NBins,Chart):-
-  obs(O),
-  maplist(to_val,O,O1),
-  length(O1,N),
-  mc_lw_sample_arg(dp_n_values(0,N,10.0,T),dp_n_values(0,N,10.0,O1),Samples,T,L),
-  maplist(to_list,L,L1),
-  append(L1,Vs),
-%  L=[Vs-_],
-  histogram(Vs,NBins,Chart).
-%   densities(L0,L,NBins,Chart).
-
-to_list(L-W,L1):-
-  maplist(app_w(W),L,L1).
-
-app_w(W,V-_,V-W).
-to_val(V,[V]-1).
-% plot the density of the state at time 1 in case of no observation (prior)
-% and in case of observing 2.5.
-% Observation as in Russel and Norvig 2010, Fig 15.10
-
-/** <examples>
-?- dens_lw(1000,40,G).
-% plot the density of the state at time 1 in case of no observation (prior)
-% and in case of observing 2.5 by taking 1000 samples and dividing the domain
-% in 40 bins
-?- post(100,40,G).
-?- hist(100,40,G).
-?- hist_val(100,40,G).
-?- hist_repeated(100,40,G).
-% plot the density of the state at time 1 in case of no observation
-% by taking 1000 samples and dividing the domain
-% in 40 bins
 
 
-*/
- 
