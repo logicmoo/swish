d765ae7e591c1bda8fd4583db45547e4307d2cd9
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-08-25 12:51:18 +0200

    filtering graphs

--- examples/inference/kalman_filter.pl
+++ examples/inference/kalman_filter.pl
@@ -31,18 +31,23 @@ Third Edition, Prentice Hall, Figure 15.10 page 587
 :- mc.
 :- begin_lpad.
 
-kf(N,O, T) :-
+kf_fin(N,O, T) :-
   init(S),
-  kf_part(0, N, S,O,T).
+  kf_part(0, N, S,O,_LS,T).
 
-kf_part(I, N, S,[V|RO], T) :-
+
+kf(N,O,LS) :-
+  init(S),
+  kf_part(0, N, S,O,LS,_T).
+
+kf_part(I, N, S, [V|RO], [S|LS], T) :-
   I < N, 
   NextI is I+1,
   trans(S,I,NextS),
   emit(NextS,I,V),
-  kf_part(NextI, N, NextS,RO, T).
+  kf_part(NextI, N, NextS, RO, LS, T).
 
-kf_part(N, N, S, [],S).
+kf_part(N, N, S, [], [], S).
 
 trans(S,I,NextS) :-
   {NextS =:= E + S},
@@ -62,18 +67,50 @@ obs_err(_,E):gaussian(E,0,1).
 :- end_lpad.
 
 hist(Samples,NBins,Chart):-
-  mc_sample_arg(kf(1,_O1,Y),Samples,Y,L0),
+  mc_sample_arg(kf_fin(1,_O1,Y),Samples,Y,L0),
   histogram(L0,NBins,Chart).
 
 dens_lw(Samples,NBins,Chart):-
-  mc_sample_arg(kf(1,_O1,Y),Samples,Y,L0),
-  mc_lw_sample_arg(kf(1,_O2,T),kf(1,[2.5],_T),Samples,T,L),
+  mc_sample_arg(kf_fin(1,_O1,Y),Samples,Y,L0),
+  mc_lw_sample_arg(kf_fin(1,_O2,T),kf_fin(1,[2.5],_T),Samples,T,L),
   densities(L0,L,NBins,Chart).
 % plot the density of the state at time 1 in case of no observation (prior)
 % and in case of observing 2.5.
 % Observation as in Russel and Norvig 2010, Fig 15.10
 
+filter(Samples,NBins,C):-
+  sample_trajectory(4,O,St),
+  mc_lw_sample_arg(kf(4,_O,T),kf_fin(4,O,_T),Samples,T,L),
+  maplist(separate,L,T1,T2,T3,T4),
+  density(T1,NBins,C1),
+  density(T2,NBins,C2),
+  density(T3,NBins,C3),
+  density(T4,NBins,C4),
+  [[x|X1],[dens|S1]]=C1.data.columns,
+  [[x|X2],[dens|S2]]=C2.data.columns,
+  [[x|X3],[dens|S3]]=C3.data.columns,
+  [[x|X4],[dens|S4]]=C4.data.columns,
+  append([S1,S2,S3,S4],AllD),
+  max_list(AllD,Max),
+  maplist(is,Y,[Max+50,Max+100,Max+150,Max+200]),
+  C = c3{data:_{xs:_{t:xt,o:xo,s1:x1,s2:x2,s3:x3,s4:x4},
+  columns:[[xt|St],[t|Y],
+    [xo|O],[o|Y],
+    [x1|X1],[s1|S1],
+    [x2|X2],[s2|S2],
+    [x3|X3],[s3|S3],
+    [x4|X4],[s4|S4]],
+    types:_{s1: spline,s2: spline,s3: spline,s4: spline,t:scatter,o:scatter}},
+ % legend:_{show: false},
+  axis:_{ x:_{ tick:_{fit:false}}}}.
+
+separate([S1,S2,S3,S4]-W,S1-W,S2-W,S3-W,S4-W).
+
+sample_trajectory(N,Ob,St):-
+  mc_sample_arg(kf(N,O,T),1,(O,T),L),
+  L=[[(Ob,St)]-_].
 /** <examples>
+?- filter(1000,40,C).
 ?- dens_lw(1000,40,G).
 % plot the density of the state at time 1 in case of no observation (prior)
 % and in case of observing 2.5 by taking 1000 samples and dividing the domain
