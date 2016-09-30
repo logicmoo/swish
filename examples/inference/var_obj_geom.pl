a339d8761f2f68c58228db64c4d77a0328e81b38
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-09-30 20:30:01 +0200

    numObj now with arity 1

--- examples/inference/var_obj_geom.pl
+++ examples/inference/var_obj_geom.pl
@@ -41,14 +41,14 @@ obj(I):-
 % expected result ~ 0.002666
 ?- mc_prob_bar(obj(5),P). % what is the probability that object 5 exists?
 % expected result ~ 0.002666
-?- mc_prob(numObj(0,2),P). % what is the probability that there are 2 objects?
+?- mc_prob(numObj(2),P). % what is the probability that there are 3 objects?
 % expected result ~ 0.0656
-?- mc_prob(numObj(0,5),P). % what is the probability that there are 5 objects?
+?- mc_prob(numObj(5),P). % what is the probability that there are 6 objects?
 % expected result ~ 0.0014
 ?- mc_sample(obj(5),1000,T,F,P). % take 1000 samples of obj(5)
 ?- mc_sample_bar(obj(5),1000,Chart). % take 1000 samples of obj(5)
-?- mc_sample_arg_bar(numObj(0,N),100,N,Chart). % take 100 samples of L in
-% findall(N,numObj(0,N),L)
+?- mc_sample_arg_bar(numObj(N),100,N,Chart). % take 100 samples of L in
+% findall(N,numObj(N),L)
 ?- mc_sample_arg_bar(obj(I),100,I,Chart). % take 100 samples of L in
 % findall(I,obj(I),L)
 ??- mc_sample_arg(obj(I),100,I,Values). % take 100 samples of L in 
