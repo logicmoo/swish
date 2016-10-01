ec51156cf66feecb76d74ab003921ba220972c19
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-10-01 18:11:52 +0200

    comments for gpr

--- examples/inference/gpr.pl
+++ examples/inference/gpr.pl
@@ -1,3 +1,19 @@
+/*
+Gaussian process (GP), see https://en.wikipedia.org/wiki/Gaussian_process
+and http://arxiv.org/pdf/1505.02965v2.pdf
+
+*/
+
+/** <examples>
+?- draw_fun(sq_exp_p,C).
+?- draw_fun(sq_exp,C).
+?- draw_fun(ou,C).
+?- draw_fun(lin,C).
+?- draw_fun([1,2,3,4,5,6],min,C).
+?- draw_fun_pred(sq_exp_p,C).
+?- draw_fun_pred_exp(sq_exp_p,C).
+*/
+
 :- use_module(library(mcintyre)).
 :- use_module(library(matrix)).
 :- use_module(library(clpfd)).
@@ -8,30 +24,11 @@
 :- mc.
 :- begin_lpad.
 
-% http://arxiv.org/pdf/1505.02965v2.pdf
 
-
-gp_predict(XP,Kernel,XT,YT,YP):-
-  compute_cov(XT,Kernel,C),
-  matrix_inversion(C,C_1),
-  transpose([YT],YST),
-  matrix_multiply(C_1,YST,C_1T),
-  gp_predict_single(XP,Kernel,XT,C_1T,YP).
-
-gp_predict_single([],_,_,_,[]).
-
-gp_predict_single([XH|XT],Kernel,X,C_1T,[YH|YT]):-
-  compute_k(X,XH,Kernel,K),
-  matrix_multiply([K],C_1T,[[YH]]),
-  gp_predict_single(XT,Kernel,X,C_1T,YT).
-
-compute_k([],_,_,[]).
-
-compute_k([XH|XT],X,Ker,[HK|TK]):-
-  call(Ker,XH,X,HK),
-  compute_k(XT,X,Ker,TK).
-  
-
+%! gp(+X:list,+Kernel:atom,-Y:list) is det
+% gp, given a list of values X and a kernel name, returns in Y
+% the list of values of type f(x) where x belongs to X and f is 
+% a function sampled from the Gaussian process
 gp(X,Kernel,Y):-
   compute_cov(X,Kernel,C),
   gp(C,Y).
@@ -54,7 +51,6 @@ cov([XH|XT],N,Ker,[KH|KY],[KHND|KYND]):-
   list0(N1,KH0),
   cov_row(XT,XH,Ker,KH1),
   call(Ker,XH,XH,KXH),
-%  kernel(XH,XH,KXH),
   append([KH0,[KXH],KH1],KH),
   append([KH0,[0],KH1],KHND),
   cov(XT,N,Ker,KY,KYND).
@@ -65,6 +61,34 @@ cov_row([H|T],XH,Ker,[KH|KT]):-
   call(Ker,H,XH,KH),
   cov_row(T,XH,Ker,KT).
 
+%! gp_predict(+XP:list,+Kernel:atom,+XT:list,+YT:list,-YP:list) is det
+% Given that the points described by the lists XT and YP and a Kernel,
+% predict the Y values of points with X values in XP and returns them in YP.
+% Prediction is performed by Gaussian process regression
+gp_predict(XP,Kernel,XT,YT,YP):-
+  compute_cov(XT,Kernel,C),
+  matrix_inversion(C,C_1),
+  transpose([YT],YST),
+  matrix_multiply(C_1,YST,C_1T),
+  gp_predict_single(XP,Kernel,XT,C_1T,YP).
+
+gp_predict_single([],_,_,_,[]).
+
+gp_predict_single([XH|XT],Kernel,X,C_1T,[YH|YT]):-
+  compute_k(X,XH,Kernel,K),
+  matrix_multiply([K],C_1T,[[YH]]),
+  gp_predict_single(XT,Kernel,X,C_1T,YT).
+
+compute_k([],_,_,[]).
+
+compute_k([XH|XT],X,Ker,[HK|TK]):-
+  call(Ker,XH,X,HK),
+  compute_k(XT,X,Ker,TK).
+  
+
+% list of kernels
+
+% squared exponential kernel with a prior on its parameters
 sq_exp_p(X,XP,K):-
   sigma(Sigma),
   l(L),
@@ -75,63 +99,51 @@ l(L):uniform(L,[1,2,3]).
 
 sigma(Sigma):uniform(Sigma,-2,2).
 
+% squared exponential kernel with fixed parameters: sigma=1, l=1
 sq_exp(X,XP,K):-
   K is exp(-((X-XP)^2)/2).
 
+% min kernel
 min(X,XP,K):-
   K is min(X,XP).
 
-
+% linear kernel with an additional term for the diagonal to ensure positive
+% definedness
 lin(X,X,K):-!,
   K is (X*X)+1.
 
 lin(X,XP,K):-
   K is (X*XP).
 
+% Ornstein-Uhlenbeck kernel
 ou(X,XP,K):-
   K is exp(-abs(X-XP)).
 
 :- end_lpad.
 
-%kernel(X,X,K):-!,
-%  K is 1.27^2+0.3^2.
-
- %K is (1.27^2)*exp(-((X-XP)^2)/2).
-
+%! draw_fun(+Kernel:atom,-C:dict) is det
+% draws 5 functions sampled from the Gaussian process with kernel Kernel
+% at points X=[-3,-2,-1,0,1,2,3]
 draw_fun(Kernel,C):-
   X=[-3,-2,-1,0,1,2,3],
   draw_fun(X,Kernel,C).
   
+%! draw_fun(+X:list,+Kernel:atom,-C:dict) is det
+% draws 5 functions sampled from the Gaussian process with kernel Kernel
+% at points X
 draw_fun(X,Kernel,C):-
-%  X=[-1.50,-1.00,-0.75,-0.40,-0.25,0.00],
-%  X=[-4.5,-4,-3.5,-3,-2.5,-2,-1.5,-1.00,-0.5,0,0.5,1,1.5,2,2.5,3,3.5,4,4.5],
   mc_sample_arg_first(gp(X,Kernel,Y),5,Y,L),
   numlist(1,5,LD),
   maplist(name_s,L,LD,L1),
   C = c3{data:_{x:x, columns:[[x|X]|L1] ,type:spline},
- % legend:_{show: false},
   axis:_{ x:_{ tick:_{fit:false}}}}.
 
-draw_fun_post(Kernel,C):-
-%  X=[-1.50,-1.00,-0.75,-0.40,-0.25,0.00],
-%  X=[-4.5,-4,-3.5,-3,-2.5,-2,-1.5,-1.00,-0.5,0,0.5,1,1.5,2,2.5,3,3.5,4,4.5],
-  numlist(0,10,X),
-  XT=[2,4,6,8,9],
-  YT=[1,-0.4,-0.8,0.25,0.6],
-  mc_lw_sample_arg(gp(X,Kernel,Y),gp(XT,Kernel,YT),100,Y,L),
-  keysort(L,LS),
-  reverse(LS,[Y1-_,Y2-_,Y3-_,Y4-_,Y5-_|_]),
-  C = c3{data:_{xs:_{y:xt,f1:x,f2:x,f3:x,f4:x,f5:x}, 
-  columns:[[y|YT],[xt|XT],[x|X],[f1|Y1],[f2|Y2],[f3|Y3],[f4|Y4],
-    [f5|Y5]],
-    types:_{f1: spline,f2:spline,f3:spline,f4:spline,
-    f5:spline,y:scatter}},
- % legend:_{show: false},
-  axis:_{ x:_{ tick:_{fit:false}}}}.
-
-draw_fun_postp(Kernel,C):-
-%  X=[-1.50,-1.00,-0.75,-0.40,-0.25,0.00],
-%  X=[-4.5,-4,-3.5,-3,-2.5,-2,-1.5,-1.00,-0.5,0,0.5,1,1.5,2,2.5,3,3.5,4,4.5],
+%! draw_fun_pred(+Kernel:atom,-C:dict) is det
+% Given the three points
+% XT=[2.5,6.5,8.5]
+% YT=[1,-0.8,0.6]
+% draws 5 functions predicting points with X=[0,...,10]
+draw_fun_pred(Kernel,C):-
   numlist(0,10,X),
   XT=[2.5,6.5,8.5],
   YT=[1,-0.8,0.6],
@@ -141,12 +153,14 @@ draw_fun_postp(Kernel,C):-
   C = c3{data:_{xs:_{y:xt,f1:x,f2:x,f3:x}, 
   columns:[[y|YT],[xt|XT],[x|X],[f1|Y1],[f2|Y2],[f3|Y3]],
     types:_{f1: spline,f2:spline,f3:spline,y:scatter}},
- % legend:_{show: false},
   axis:_{ x:_{ tick:_{fit:false}}}}.
 
-draw_fun_poste(Kernel,C):-
-%  X=[-1.50,-1.00,-0.75,-0.40,-0.25,0.00],
-%  X=[-4.5,-4,-3.5,-3,-2.5,-2,-1.5,-1.00,-0.5,0,0.5,1,1.5,2,2.5,3,3.5,4,4.5],
+%! draw_fun_pred_exp(+Kernel:atom,-C:dict) is det
+% Given the three points
+% XT=[2.5,6.5,8.5]
+% YT=[1,-0.8,0.6]
+% draws the expected prediction for points with X=[0,...,10]
+draw_fun_pred_exp(Kernel,C):-
   numlist(0,10,X),
   XT=[2.5,6.5,8.5],
   YT=[1,-0.8,0.6],
@@ -154,20 +168,6 @@ draw_fun_poste(Kernel,C):-
   C = c3{data:_{xs:_{y:xt,f:x}, 
   columns:[[y|YT],[xt|XT],[x|X],[f|Y]],
     types:_{f: spline,y:scatter}},
- % legend:_{show: false},
-  axis:_{ x:_{ tick:_{fit:false}}}}.
-
-draw_fun_postee(Kernel,C):-
-%  X=[-1.50,-1.00,-0.75,-0.40,-0.25,0.00],
-%  X=[-4.5,-4,-3.5,-3,-2.5,-2,-1.5,-1.00,-0.5,0,0.5,1,1.5,2,2.5,3,3.5,4,4.5],
-  numlist(0,10,X),
-  XT=[2.5,6.5,8.5],
-  YT=[1,-0.8,0.6],
-  mc_lw_expectation(gp(X,Kernel,Y),gp(XT,Kernel,YT),5,Y,YE),
-  C = c3{data:_{xs:_{y:xt,f:x}, 
-  columns:[[y|YT],[xt|XT],[x|X],[f|YE]],
-    types:_{f: spline,y:scatter}},
- % legend:_{show: false},
   axis:_{ x:_{ tick:_{fit:false}}}}.
 
 compute_e([],_,_,_,[]).
@@ -178,13 +178,4 @@ compute_e([X|T],Kernel,XT,YT,[YE|TYE]):-
 name_s(V-_,N,[ND|V]):-
   atomic_concat(f,N,ND).
 
-/** <examples>
-?- draw_fun(sq_exp_p,C).
-?- draw_fun(sq_exp,C).
-?- draw_fun(ou,C).
-?- draw_fun(lin,C).
-?- draw_fun([1,2,3,4,5,6],min,C).
-?- draw_fun_post(sq_exp_p,C).
-?- draw_fun_postp(sq_exp_p,C).
-?- draw_fun_poste(sq_exp_p,C).
-*/
+
