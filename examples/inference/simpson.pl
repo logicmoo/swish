5de17fd5e21653f6621595fa49f815328924e85f
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-11-20 19:10:28 +0100

    causality examples

--- examples/inference/simpson.pl
+++ examples/inference/simpson.pl
@@ -1,9 +1,7 @@
 :- use_module(library(pita)).
 
 :- if(current_predicate(use_rendering/1)).
-:- use_rendering(c3).
 :- use_rendering(graphviz).
-:- use_rendering(table,[header(['Multivalued variable index','Rule index','Grounding substitution'])]).
 :- endif.
 
 :- pita.
