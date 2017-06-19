16275c7f1538c8bd383b6ba08708d0f119c36627
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2017-06-19 08:58:21 +0200

    dp_r now working

--- examples/inference/dirichlet_process_R.pl
+++ examples/inference/dirichlet_process_R.pl
@@ -88,8 +88,9 @@ pick_portion(_,_,P):P;neg_pick_portion(_,_,P):1-P.
 :- end_lpad.
 
 hist(Samples,NBins):-
-  mc_sample_arg_first(dp_stick_index(1,10.0,V),Samples,V,L),
-  histogram_r(L,NBins).
+  mc_sample_arg_raw(dp_stick_index(1,10.0,V),Samples,V,L),
+  findall(Val-1,member(Val,L),LV),
+  histogram_r(LV,NBins).
 
 hist_repeated_indexes(Samples,NBins):-
   repeat_sample(0,Samples,L),
