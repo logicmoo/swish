c639b8ac5b8b9e41910afb076cd86d2d7d616e9e
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2018-10-27 19:45:25 +0200

    dependency graph

--- examples/inference/h_index.pl
+++ examples/inference/h_index.pl
@@ -1,6 +1,8 @@
-/* computation of the h-index of an author given his number of papers
+/* Computation of the h-index of an author given his number of papers
 assuming that the citations per paper follow a Poisson distribution with 
 given average.
+It shows that the expected h-index is directly proportional to the
+average number of citations (expected h-index=avg. n. cit. +5)
 */
 :- use_module(library(mcintyre)).
 :- if(current_predicate(use_rendering/1)).
@@ -38,6 +40,14 @@ compute_index([H|T],I0,I):-
     
 :- end_lpad.
 
+h_vs_avg_cit(Papers,MaxAvg,Chart):-
+  findall(E,(
+    between(1,MaxAvg,N),mc_expectation(h_index(Papers,N,T),10,T,E)
+  ),V),
+  numlist(1,MaxAvg,X),
+  findall(N,(between(1,MaxAvg,N0),N is 5+N0),Dep),
+  Chart=c3{data:_{x:x, columns:[[x|X],['Expected h-index'|V],['Dependency'|Dep]]}}.
+
 /** <examples> Your example queries go here, e.g.
 
 ?- mc_sample_arg_first(h_index(200,10,H),1000,H,HList),density(HList,Dens,[nbins(20)]).
@@ -47,5 +57,10 @@ compute the distribution of the h_index given that the authors wrote
 compute the expected value of the h_index given that the authors wrote
 200 papers and each paper receives on average 10 citations
 ?- mc_sample_arg_first(citations(10,200,Cit),1000,Cit,CitList),density(CitList,Dens,[nbins(20)]).
+?- h_vs_avg_cit(100,40,Chart).
+Plot of the depedency of the expected h-index as a function of the average
+number of citations per article (max 40) given that there are 100 papers.
+The graphs shows that the expected h-index is directly proportional to the
+average number of citations (expected h-index=avg. n. cit. +5)
 */
 
